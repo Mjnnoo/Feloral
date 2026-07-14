0749ab0b36c05c1f06a66cd8c8a6c741b1dbd48c
MJN <you@example.com>
2026-07-14 16:14:40 +0330

    security: move JWT secret to environment variables

--- Desktopfeloral-backend/src/auth/auth.module.ts
+++ Desktopfeloral-backend/src/auth/auth.module.ts
@@ -1,40 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
-
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy/jwt.strategy';
 
 @Module({
   imports: [
     PrismaModule,
     UsersModule,
-
     PassportModule,
 
-    JwtModule.register({
-      secret: 'feloral_super_secret_key_2026',
-      signOptions: {
-        expiresIn: '30d',
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
+        signOptions: {
+          expiresIn: '15m',
+        },
+      }),
     }),
   ],
-
   controllers: [AuthController],
-
-  providers: [
-    AuthService,
-    JwtStrategy,
-  ],
-
-  exports: [
-    AuthService,
-    JwtModule,
-  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
